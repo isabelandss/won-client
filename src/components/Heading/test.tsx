9b778533ba5a259b4e16031fbf355f62ca416158
Isabela Neri <isabelandss@gmail.com>
2021-05-29 17:47:27 -0300

    heading components

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,10 +1,37 @@
-import { render } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
 
 import Heading from '.'
+import theme from 'styles/theme'
 
 describe('<Heading />', () => {
-  test('should match snapshot', () => {
-    const { container } = render(<Heading />)
-    expect(container.firstChild).toMatchSnapshot()
+  test('should render a white heading by default', () => {
+    renderWithTheme(<Heading>lorem ipsum</Heading>)
+    expect(screen.getByRole('heading', { name: /lorem ipsum/i })).toHaveStyle({
+      color: theme.colors.white,
+    })
+  })
+
+  test('should render a black when color is passed', () => {
+    renderWithTheme(<Heading color="black">lorem ipsum</Heading>)
+    expect(screen.getByRole('heading', { name: /lorem ipsum/i })).toHaveStyle({
+      color: theme.colors.black,
+    })
+  })
+
+  test('should render a left line', () => {
+    renderWithTheme(<Heading lineLeft>lorem ipsum</Heading>)
+    expect(screen.getByRole('heading', { name: /lorem ipsum/i })).toHaveStyle({
+      'border-left': `0.7rem solid ${theme.colors.secondary}`,
+    })
+  })
+
+  test('should render a bottom line', () => {
+    renderWithTheme(<Heading lineBottom>lorem ipsum</Heading>)
+    expect(
+      screen.getByRole('heading', { name: /lorem ipsum/i }),
+    ).toHaveStyleRule('border-bottom', `0.5rem solid ${theme.colors.primary}`, {
+      modifier: '::after',
+    })
   })
 })
