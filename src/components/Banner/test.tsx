c15f8a5e95d24450b1ca3122f800901a1578b404
Isabela Neri <isabelandss@gmail.com>
2021-05-31 15:16:05 -0300

    componente banner

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,47 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong></p> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death',
+}
+
+describe('<Banner />', () => {
+  describe('should render correctly', () => {
+    let bannerContainer: HTMLElement
+
+    beforeEach(() => {
+      const { container } = renderWithTheme(<Banner {...props} />)
+      bannerContainer = container
+    })
+
+    test('should match snapshot', () => {
+      expect(bannerContainer.firstChild).toMatchSnapshot()
+    })
+
+    test('should render image', () => {
+      expect(screen.getByRole('img')).toBeInTheDocument()
+    })
+
+    test('should render title', () => {
+      expect(screen.getByText(/defy death/i)).toBeInTheDocument()
+    })
+
+    test('should render subtitle', () => {
+      expect(
+        screen.getByRole('heading', {
+          name: /Play the new CrashLands season/i,
+        }),
+      ).toBeInTheDocument()
+    })
+
+    test('should render button link', () => {
+      expect(screen.getByRole('link')).toBeInTheDocument()
+    })
+  })
+})
